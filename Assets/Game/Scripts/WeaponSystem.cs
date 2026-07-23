a54f44c9712109071755001b7f096e5662a4e7b4
lin-php <mann.philipp93@gmail.com>
2026-07-23 16:18:58 +0200

    feat: add weapon attack cooldown

--- Assets/Game/Scripts/WeaponSystem.cs
+++ Assets/Game/Scripts/WeaponSystem.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class WeaponSystem : MonoBehaviour
 {
-   
+    // [SerializeField] private WeaponData equippedWeapon;
+    private float nextAttackTime;
+    [SerializeField] private float attackCooldown = 2f;
+
     public void TryAttack()
     {
-        Debug.Log("Attack reached Weapon System");
+        
+        if (Time.time < nextAttackTime) 
+        {
+            return;
+        }
+
+        nextAttackTime = Time.time + attackCooldown;
+
+        PerformAttack();
     }
+
+    public void PerformAttack()
+    {
+        Debug.Log("Angriff ausgeführt!");
+    }
+
 
 
 }
